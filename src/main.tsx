58da11bc016facbde7a5160098c9022db3d8ceb4
Alex Ercolin <aoliveira@rhei.com>
2026-01-20 12:00:10 -0300

    Enable MSW in production for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,12 @@ import App from './App';
 import '../index.css';
 
 async function enableMocking() {
-  if (process.env.NODE_ENV !== 'development') {
-    return;
-  }
-
   const { worker } = await import('./mocks/browser');
 
   return worker.start({
+    serviceWorker: {
+      url: '/dynamic-step-form-poc/mockServiceWorker.js',
+    },
     onUnhandledRequest: 'bypass',
   });
 }
